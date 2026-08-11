745d36c2e98f583d2fbc4900aa4945c428c8ebf7
enzomakenzy <enzomqueiroz@gmail.com>
2026-08-11 17:10:06 -0300

    feat: add header component

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,9 @@
+import { Header } from "./components/header";
+
 export default function App() {
   return (
     <main className="bg-hero-mesh h-screen">
-      dsadsa
+      <Header />
     </main>
   );
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,8 @@
+export function Header() {
+  return (
+    <div className="text-center pt-8">
+      <h2 className="uppercase text-sm/8 text-muted-foreground tracking-[0.24rem] font-normal scale-y-90">Previsão do tempo</h2>
+      <h1 className="text-5xl tracking-tight font-header font-semibold bg-linear-to-r from-brand-start to-brand-end bg-clip-text text-transparent md:text-6xl">Nimbo</h1>
+    </div>
+  );
+}
